44fda84ccdcfb049048c32cd3840960f1ba49407
Mukul <kmukul1197@gmail.com>
2021-07-01 11:39:43 +0530

    Set ejs as templating engine with express

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
 app.get("/", (req, res) =>{
-	res.send('Welcome to solutionBuddy!');
+	// res.send('Welcome to solutionBuddy!');
+	res.render('root');
 })
 app.listen(port, () =>{
 	console.log(`Example app listening at http://localhost:${port}`);
